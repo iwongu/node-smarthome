7143dd24e010455e32df9f0c3959e28046a23ccf
Wongoo Lee <iwongu@gmail.com>
2015-02-27 21:36:44 -0800

    Indentions.

--- detector.js
+++ detector.js
@@ -11,40 +11,39 @@ var low_in;
 var take_low_time;
 
 function setup(callback) {
-    pubsub('detector/living_room/1').setValue({status: false});
-    gpio.close(pir_pin, function(err) {
-	console.log('calibrating sensor for ' + calibration_time + ' seconds');
-	gpio.open(pir_pin, 'in', function(err) {
-	    setTimeout(callback, calibration_time * 1000);
-	});
+  gpio.close(pir_pin, function(err) {
+    console.log('calibrating sensor for ' + calibration_time + ' seconds');
+    gpio.open(pir_pin, 'in', function(err) {
+      setTimeout(callback, calibration_time * 1000);
     });
+  });
 }
 
 function loop(value) {
-    gpio.read(pir_pin, function(err, value) {
-      if (value) {
-	  if (lock_now) {
-	      lock_now = false;
-	      console.log('motion detected');
-	      pubsub('detector/living_room/1').setValue({status: true});
-	  }
-	  take_low_time = true;
-      } else {
-	  if (take_low_time) {
-	      low_in = Date.now();
-	      take_low_time = false;
-	  }
-	  if (!lock_now && Date.now() - low_in > pause) {
-	      lock_now = true;
-	      console.log('motion ended');
-	      pubsub('detector/living_room/1').setValue({status: false});
-	  }
+  gpio.read(pir_pin, function(err, value) {
+    if (value) {
+      if (lock_now) {
+	lock_now = false;
+	console.log('motion detected');
+	pubsub('detector/living_room/1').setValue({status: true});
       }
-    });
+      take_low_time = true;
+    } else {
+      if (take_low_time) {
+	low_in = Date.now();
+	take_low_time = false;
+      }
+      if (!lock_now && Date.now() - low_in > pause) {
+	lock_now = true;
+	console.log('motion ended');
+	pubsub('detector/living_room/1').setValue({status: false});
+      }
+    }
+  });
 }
 
 setup(function() {
-    console.log('calibrating done');
-    setInterval(loop, 50);
+  console.log('calibrating done');
+  setInterval(loop, 50);
 });
 
--- light.js
+++ light.js
@@ -6,20 +6,20 @@ var gpio = require('pi-gpio');
 var relay_pin = 7;
 
 function setup(callback) {
-    gpio.close(relay_pin, function(err) {
-	gpio.open(relay_pin, 'out', function(err) {
-	    gpio.write(relay_pin, 1, function(err) {
-		callback();
-	    });
-	});
+  gpio.close(relay_pin, function(err) {
+    gpio.open(relay_pin, 'out', function(err) {
+      gpio.write(relay_pin, 1, function(err) {
+	callback();
+      });
     });
+  });
 }
 
 setup(function() {
-    pubsub('light/living_room/1').
-	on('value', function(data) {
-	    console.log('light turned ' + (data.value.status ? 'on' : 'off'));
-	    gpio.write(relay_pin, data.value.status ? 0 : 1, function(err) {
-	    });
-	});
+  pubsub('light/living_room/1').
+    on('value', function(data) {
+      console.log('light turned ' + (data.value.status ? 'on' : 'off'));
+      gpio.write(relay_pin, data.value.status ? 0 : 1, function(err) {
+      });
+    });
 });
